4fb9a6752910399017f6a8a9d28cf2d2349ecb95
brutik <thoughtsoleg89@gmail.com>
2022-07-19 17:45:36 +0200

    Initial Commit

---
+++ URLSession2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSession2
+//
+//  Created by Aleh Kuchynski on 19/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
