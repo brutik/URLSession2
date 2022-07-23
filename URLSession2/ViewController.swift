648e5b27779a294a89581931310d186f1b5990d2
brutik <thoughtsoleg89@gmail.com>
2022-07-23 20:16:16 +0200

    Get recuest

--- URLSession2/ViewController.swift
+++ URLSession2/ViewController.swift
@@ -2,25 +2,35 @@
 //  ViewController.swift
 //  URLSession2
 //
-//  Created by Aleh Kuchynski on 19/07/2022.
+//  Created by Aleh Kuchynski on 23/07/2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-   
-    @IBAction func getRequest(_ sender: Any) {
-        
+    
+    
+    @IBAction func getRecuest(_ sender: Any) {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
         let session = URLSession.shared
         session.dataTask(with: url) { (data, response, error) in
-            guard let response = response else { return }
-        }
-    }
+            guard let response = response,
+            let data = data
+            else { return }
+            print(response)
+            print(data)
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            }catch{
+                print(error)
+            }
+        }.resume()
+       }
     
-    @IBAction func postRequest(_ sender: Any) {
+    @IBAction func postRecuest(_ sender: Any) {
+        
     }
     
 }
-
