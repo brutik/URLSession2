d41e1be185861b0d26b5e1bf226c395dac83202e
brutik <thoughtsoleg89@gmail.com>
2022-07-24 17:06:07 +0200

    Add post recuest

---
+++ URLSession2/CoursesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewController.swift
+//  URLSession2
+//
+//  Created by Aleh Kuchynski on 24/07/2022.
+//
+
+import UIKit
+
+class CoursesViewController: UIViewController {
+
+    
+    @IBOutlet var tableView: UITableView!
+}
--- URLSession2/ViewController.swift
+++ URLSession2/ViewController.swift
@@ -31,6 +31,32 @@ class ViewController: UIViewController {
     
     @IBAction func postRecuest(_ sender: Any) {
         
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let userData = ["Course": "Networking", "lesson": "Get and Post Requests "]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData, options: []) else { return }
+        request.httpBody = httpBody
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            guard let data = data, let response = response else { return }
+            
+            print(response)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
+        
     }
     
 }
