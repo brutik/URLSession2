0dcdc85cccb80b302221a31f8f937f9e573f7a3e
brutik <thoughtsoleg89@gmail.com>
2022-07-25 20:53:18 +0200

    JSON

--- URLSession2/CoursesViewController.swift
+++ URLSession2/CoursesViewController.swift
@@ -1,14 +1,20 @@
 //
-//  TableViewController.swift
+//  CoursesViewController.swift
 //  URLSession2
 //
-//  Created by Aleh Kuchynski on 24/07/2022.
+//  Created by Aleh Kuchynski on 25/07/2022.
 //
 
 import UIKit
 
 class CoursesViewController: UIViewController {
-
     
     @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
+    
+    
+
---
+++ URLSession2/WebViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewController.swift
+//  URLSession2
+//
+//  Created by Aleh Kuchynski on 25/07/2022.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+    
+}
